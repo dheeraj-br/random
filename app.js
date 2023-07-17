840812a6888b3fa7a1889ba6063156c6ef3a7ace
dheeraj <dheeraj.br2@gmail.com>
2023-07-18 04:43:23 +0530

    add global error handling

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 import express from 'express';
 import { router } from './route.js';
+import { globalErrorHandler, pageNotFoundHandler } from './errorHandler.js';
 
 const PORT = process.env.PORT || 3000;
+
+process.on('uncaughtException', (error) => {
+  // log errors and stack trace
+  console.log(error);
+  // gracefully shutdown
+  //
+});
+
+process.on('unhandledRejection', (error) => {
+  // log errors and stack trace
+  console.log(error);
+  // gracefully shutdown
+  //
+});
 
 let app = express();
 
 app.use('/', router);
+
+app.all('*', pageNotFoundHandler);
+
+app.use(globalErrorHandler);
 
 app.listen(PORT);
---
+++ errorHandler.js
@@ -0,0 +1,54 @@
+import httpStatus from 'http-status';
+
+export class CustomError extends Error {
+  constructor(message, statusCode, isOperationalError = false) {
+    super(message);
+    this.message = message;
+    this.statusCode = statusCode;
+    this.isOperationalError = isOperationalError;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export function pageNotFoundHandler(req, res, next) {
+  const error = new CustomError(
+    `cannot find ${req.originalUrl}`,
+    httpStatus.NOT_FOUND,
+    true
+  );
+  next(error);
+}
+
+export function catchRuntimeError(controller) {
+  return (req, res, next) => {
+    try {
+      controller(req, res, next);
+    } catch (error) {
+      const customError = new CustomError(
+        error.message,
+        error.statusCode || httpStatus.INTERNAL_SERVER_ERROR,
+        error.isOperationalError
+      );
+      next(customError);
+    }
+  };
+}
+
+export function globalErrorHandler(error, req, res, next) {
+  if (error.isOperationalError) {
+    res.status(error.statusCode).json({
+      message: error.message,
+      statusCode: error.statusCode,
+      isOperationalError: error.isOperationalError,
+    });
+  } else {
+    // log errors and stack trace
+    console.log(error);
+    //
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: httpStatus['500_NAME'],
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      isOperationalError: error.isOperationalError,
+    });
+  }
+}
